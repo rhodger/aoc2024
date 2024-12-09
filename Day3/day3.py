d87ea37afc5010e66c4d1ec330ade7589a757293
rhod <rhod@rhodbox>
2024-12-09 12:33:32 +0000

    Half of d3c2 solution

--- Day3/day3.py
+++ Day3/day3.py
@@ -15,3 +15,17 @@ class SolutionD3C1(SolutionD3):
         for op in operations:
             total += op[0] * op[1]
         return total
+
+class SolutionD3C2(SolutionD3):
+    def get_operations(self):
+        valid_sections = []
+        for line in self.input:
+            valid_sections.append(re.sub(r'don\'t().*do()', 'do()', line))
+
+        print(f"valid sections: {valid_sections}")
+        
+        ops = []
+        for line in valid_sections:
+            ops += list(map(lambda x: (int(x[0]), int(x[1])), re.findall(r'mul\((\d+),(\d+)\)', line)))
+
+        return ops
