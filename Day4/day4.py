f2ca50e9be843e30f1c5e02072e5f80d718c93e4
rhod <rhod@rhodbox>
2024-12-09 17:41:20 +0000

    Solution should be working

--- Day4/day4.py
+++ Day4/day4.py
@@ -1,5 +1,44 @@
 from Solution.solution import *
 
 class SolutionD4(Solution):
+    # Get a processable grid (2D array)
     def get_formatted_input(self):
         return list(map(lambda x: list(x), self.input))
+
+class SolutionD4C1(SolutionD4):
+    # Check if a character is the first letter of a word and, if so, how many words
+    def is_first_letter_of_word(self, word, row, col):
+        word = word[1:]
+        grid = self.get_formatted_input()
+        print(f"\nNew check: {col},{row}")
+
+        def check_direction(row, col, dr, dc):
+            print("New direction")
+            for i in range(len(word)):
+                new_row, new_col = row + dr, col + dc
+                if not (0 <= new_row < len(grid) and 0 <= new_col < len(grid[0])):
+                    return False
+                print(f"Comparing to {grid[new_row][new_col]} ({new_col},{new_row})")
+                if grid[new_row][new_col] != word[i]:
+                    return False
+                row, col = new_row, new_col
+            print("Hit!")
+            return True
+
+        directions = [(-1,0), (-1,1), (0,1), (1,1), (1,0), (1,-1), (0,-1), (-1,-1)]
+
+        hits = 0
+        for dr, dc in directions:
+            if check_direction(row, col, dr, dc):
+                hits += 1
+
+        return hits
+    
+    # Find occurrences of XMAS in the input grid
+    def solve(self):
+        hits = 0
+        for y in range(len(self.input)):
+            for x in range(len(self.input[y])):
+                if self.input[y][x] == 'X':
+                    hits += self.is_first_letter_of_word('XMAS', y, x)
+        return hits
--- Day4/test_day4.py
+++ Day4/test_day4.py
@@ -14,14 +14,29 @@ MAMMMXMMMM
 MXMXAXMASX"""
 
 class TestSolutionD4(unittest.TestCase):
+    # Check initialisation works
     @patch('builtins.open', new_callable=mock_open, read_data=TEST_INPUT_1)
     def setUp(self, mock_open):
         self.d = SolutionD4('')
         mock_open.assert_called_once()
         self.assertEqual(self.d.input[0], "MMMSXXMASM")
 
+    # Check formatting of input works
     def test_get_formatted_input(self):
         f = self.d.get_formatted_input()
         
         self.assertEqual(f[0], ['M','M','M','S','X','X','M','A','S','M'])
         self.assertEqual(f[-1], ['M','X','M','X','A','X','M','A','S','X'])
+
+class TestSolutionD4C1(TestSolutionD4):
+    # Check initialisation works
+    @patch('builtins.open', new_callable=mock_open, read_data=TEST_INPUT_1)
+    def setUp(self, mock_open):
+        self.d = SolutionD4C1('')
+        mock_open.assert_called_once()
+        self.assertEqual(self.d.input[0], "MMMSXXMASM")
+
+    # Test solution works on example
+    def test_solve(self):
+        solution = self.d.solve()
+        self.assertEqual(solution, 18)
